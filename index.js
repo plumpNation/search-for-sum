17b65e13bfe099d3b41660925d52cfe88d219e46
Gavin King <gavin.king@itslearning.com>
2018-07-26 22:32:25 +0200

    Create the cloned array test data before running the tests

--- index.js
+++ index.js
@@ -11,22 +11,37 @@ const sorted = unsorted.sort((a, b) => a - b);
 
 const desired = 8;
 
+// We're using .slice() to avoid mutating the original test arrays.
+// We do it here to avoid it affecting the test results.
+// @see https://stackoverflow.com/questions/3978492/javascript-fastest-way-to-duplicate-an-array-slice-vs-for-loop
+const testData = [
+    unsorted.slice(),
+    sorted.slice(),
+    unsorted.slice(),
+    unsorted.slice(),
+    unsorted.slice(),
+    unsorted.slice(),
+    unsorted.slice()
+];
+
 suite
     .add('Quadratic `for()`', function () {
-        quadratic.for(unsorted, desired);
+        // This test simply doesn't take into consideration if the data
+        // is sorted or unsorted.
+        quadratic.for(testData[0], desired);
     })
     .add('Binary', function () {
         // must use a sorted array, so it's not comparable to bidirectional
-        binary(sorted, desired);
+        binary(testData[1], desired);
     })
     .add('Bidirectional with [] cache', function () {
-        bidirectional.array(unsorted, desired);
+        bidirectional.array(testData[3], desired);
     })
     .add('Bidirectional with {} cache', function () {
-        bidirectional.object(unsorted, desired);
+        bidirectional.object(testData[4], desired);
     })
     .add('Bidirectional with Map() cache', function () {
-        bidirectional.map(unsorted, desired);
+        bidirectional.map(testData[5], desired);
     })
     .on('cycle', function (event) {
         console.log(String(event.target));
